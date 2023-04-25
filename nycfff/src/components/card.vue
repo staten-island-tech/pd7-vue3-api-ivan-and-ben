cdb589a02a390506b499b4f4c1e8bbe116902f2b
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2023-04-25 13:10:05 -0400

    Video 1 done

---
+++ nycfff/src/components/card.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="card">
+      <h2>{{ baby.nm }}</h2>
+    </div>
+</template>
+
+<script setup>
+const props = {
+  baby: Object,
+  id: Number,
+}
+</script>
+ 
+<style scoped>
+
+</style>
--- nycfff/src/views/AboutView.vue
+++ nycfff/src/views/AboutView.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
-
+   
   </div>
 </template>
 
-<script>
-  export default {
-    
-  }
+<script setup>
+import {ref, onMounted} from 'vue'
+import card from '../components/card.vue';
+const babyNames = ref("")
+async function getBabies(){
+  let res = await fetch('https://data.cityofnewyork.us/resource/25th-nujf.json')
+  let data = await res.json()
+  babyNames.value = data
+}
+onMounted(()=>{
+ getBabies()
+})
 </script>
 
 <style lang="scss" scoped>
