c72d8eba6bbc15975d7cce35bc7d5950937518ea
RodentCoder <ichen080405@gmail.com>
2023-05-01 19:16:35 -0400

    IT WORKS YOOO

--- nycfff/src/views/HomeView.vue
+++ nycfff/src/views/HomeView.vue
@@ -1,27 +1,16 @@
 <template>
-  <BarChart />
-  <h1>Hello</h1>
-  <div class="container">
-    <NYCdataprop v-for="cause in test" :cause="cause" />
-  </div>
+  <h1 class="title"> Air Qulaity In NYC</h1>
+<BarChart />
+
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue";
-import NYCdataprop from "../components/NYCdataprop.vue";
+
 import BarChart from "../components/BarChart.vue";
-const test = ref("");
-async function getCause() {
-  let res = await fetch(
-    "https://data.cityofnewyork.us/resource/jb7j-dtam.json"
-  );
-  let data = await res.json();
-  test.value = data;
-  console.log(test.value);
-}
-onMounted(() => {
-  getCause();
-});
 </script>
 
-<style scoped></style>
+<style scoped>
+.title{
+  font-size: x-large;
+  text-align: center;
+}</style> 
