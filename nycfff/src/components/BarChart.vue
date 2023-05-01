d4ffc4ddd70ba1717aaf288b50a619aefeedea54
RodentCoder <ichen080405@gmail.com>
2023-05-01 12:37:58 -0400

    mmmm

--- nycfff/src/components/BarChart.vue
+++ nycfff/src/components/BarChart.vue
@@ -1,42 +1,41 @@
 <template>
-  <Bar
-    id="my-chart-id"
-    :options="chartOptions"
-    :data="chartData"
-  />
+  <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
 </template>
 
 <script>
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import { Bar } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from "chart.js";
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-
-async function getCause() {
-  let res = await fetch(
-    "https://data.cityofnewyork.us/resource/jb7j-dtam.json"
-  );
-  let data = await res.json();
-  test.value = data;
-  let filter = data.filter((yes)=> yes.sex === "Male");
-  let thing = filter
-  console.log(thing)
-  console.log(test.value); 
-}
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+);
 
 export default {
-  name: 'BarChart',
+  name: "BarChart",
   components: { Bar },
   data() {
     return {
       chartData: {
-        labels: [ 'January', 'February', 'March' ],
-        datasets: [ { data: [40, 20, 12] } ]
+        labels: ["January", "February", "March"],
+        datasets: [{ data: [40, 20, 12] }],
       },
       chartOptions: {
-        responsive: true
-      }
-    }
-  }
-}
+        responsive: true,
+      },
+    };
+  },
+};
 </script>
